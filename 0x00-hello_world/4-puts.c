e3a45792b5dd38dca459fe7042b8ff406bf757ec
elaine <emuhombe@gmail.com>
2022-09-08 20:07:12 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,9 @@
 *using puts function
 */
 #include<stdio.h>
-/*
- * main entry point
- * Description:executes the puts function
- * Return:Always 0 (success)
- */
+
 int main(void)
+/* This main functin executes the puts () function */
 {
  puts("\"Programming is like building a multilingual puzzle");
  return (0);	
