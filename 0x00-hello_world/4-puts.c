1198cccf8db1e051bf2e1324a92093bc2d0d1b15
elaine <emuhombe@gmail.com>
2022-09-08 19:14:48 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
+/*This program uses puts function to output a string */
 #include <stdio.h>
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts(""Programming is like building a multilingual puzzle");
 	
 	return (0);	
 }
