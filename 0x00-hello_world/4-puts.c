4c25dad5cf545a5539b8fe0a903f94022cc6d48a
elaine <emuhombe@gmail.com>
2022-09-08 19:59:10 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,12 @@
 *This is a C program to output a string
 *using puts function
 */
-#include <stdio.h>
-/*function main.c*/
+#include<stdio.h>
+/*
+ * main entry point
+ * Description: output a string using puts function
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
  puts("\"Programming is like building a multilingual puzzle");
