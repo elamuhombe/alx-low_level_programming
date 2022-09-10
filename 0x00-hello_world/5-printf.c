94f7b2c53b71bdf2752d330d0d23bf5dfb3bd721
elaine <emuhombe@gmail.com>
2022-09-10 02:09:49 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,7 @@
  * Return: Always 0 (Success)
  */
 #include<stdio.h>
+/* betty style doc for function main goes there */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
