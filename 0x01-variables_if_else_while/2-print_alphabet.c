ea558884250c97322d486f19055fc7346545f040
elaine <emuhombe@gmail.com>
2022-09-12 11:37:31 -0700

    lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all lowercase letters
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	char c
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			putchar(c"\n");
+		}
+	return (0);
+}
