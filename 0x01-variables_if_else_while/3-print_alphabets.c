07c4e82c75ba7334101cc754ae6ff95c60207204
elaine <emuhombe@gmail.com>
2022-09-12 01:38:09 -0700

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,14 +4,14 @@ int main(void)
 {
 		char l;
 
-		printf("Lowercase letters: \n");
+		printf("Lowercase letters:");
 
 		for (l='a'; l <= 'z'; l++)
 		{
 			printf("%c ", l);
 		}
 
-		printf("\nUppercase letters : \n");
+		
 		for (l='A'; l <= 'Z'; l++)
 		{
 			printf("%c ", l);
