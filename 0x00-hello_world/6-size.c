05541f886587063f434744faeedd933ad246cf17
elaine <emuhombe@gmail.com>
2022-09-12 00:09:45 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,8 +19,8 @@ int main(void)
 
 	printf("Size of a char: %zu bytes\n", sizeof(charType));
 	printf("Size of an int %zu bytes\n", sizeof(intType));
-	printf("Size of a long int %zu bytes\n", sizeof(longInt));
-	printf("Size of a long long int %zu bytes\n", sizeof(longlongintType));
+	printf("Size of a long int %ld bytes\n", sizeof(longInt));
+	printf("Size of a long long int %lld bytes\n", sizeof(longlongintType));
 	printf("Size of a float %zu bytes \n", sizeof(floatType));
 	return (0);
 }
