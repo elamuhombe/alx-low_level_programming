e205a1a8f015dbb8891d5f6def12141ee5101a03
elaine <emuhombe@gmail.com>
2022-09-12 09:57:45 -0700

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,11 +3,11 @@
 int main(void)
 {	
 	char ch;
-	
+	char CH;
 
-	for (ch='a'; ch <= 'z'; ch++)
+	for (ch='a'; ch <= 'z'; ch++ && CH='A'; CH <= 'Z', CH++)
 	{
-		putchar(ch);
+		putchar(ch, CH);
 	}
 
 	return (0);
