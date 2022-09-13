61b76347cdc20e23f60fcb7b06b3b180a618971b
elaine <emuhombe@gmail.com>
2022-09-13 10:07:52 -0700

    reverse alphabets

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,8 +3,10 @@
  * main - Entry point
  *
  * Description: program that prrints lower case 
- * alphabets in reverse order followed by a new line
+ * alphabets in reverse order
+ * followed by a new line
  *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -12,7 +14,7 @@ int main(void)
 
 	for(ch = 'a'; ch <= 'z'; ch --)
 	{
-		printf("%c", ch);
+		printf("%c",i ch);
 	}
 	return (0);
 }
