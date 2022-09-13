f1857e580e8795baf8b1773324a4e194ff946cb0
elaine <emuhombe@gmail.com>
2022-09-13 07:45:53 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
-/* More headers go  there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: program that prints 'with proper grammar,
+ * but the outcome is a piece of art followed
+ * by a new line
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
