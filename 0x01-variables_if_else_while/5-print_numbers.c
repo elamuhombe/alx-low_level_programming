40b790fd783b66bdb8b53b142ef0c25ce5d5c178
elaine <emuhombe@gmail.com>
2022-09-13 08:46:34 -0700

    base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Program that prints all the single single
+ * digits of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	int i;
+	
+	for (i= 0; i<=9; i++)
+	{
+		printf("\n" , i);
+	}
+	return (0);
+}
