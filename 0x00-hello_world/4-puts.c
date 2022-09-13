283c0120a91ef64671537def57fbb4b44a9c0ce7
elaine <emuhombe@gmail.com>
2022-09-13 07:06:12 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,13 +4,12 @@
  * main - Entry point
  *
  * Description: program that prints exactly: 
- * 'Programming is like building a multilingual puzzle'
+ *'Programming is like building a multilingual puzzle'
  * and displays it in a new line
  *
  * Return: Always 0 (Success)
  *
  */
-
 int main(void)
 {
  puts("\"Programming is like building a multilingual puzzle");
