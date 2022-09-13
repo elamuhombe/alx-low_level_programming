39b36ff022c51b971033d0a93646f824487b5373
elaine <emuhombe@gmail.com>
2022-09-13 04:39:06 -0700

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: program prints the all alphabet apart 
+ * fro q and e in lower case 
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main (void)
+{
+	char ch;
+
+	for (ch= 'a'; ch<= 'z'; ch++)
+	{
+		char *ch = ch+5;
+		char *ch = ch+17;
+		putchar(ch);
+	}
+
