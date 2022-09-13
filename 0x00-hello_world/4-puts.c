60800e425f809e67a8b1782c609b6370cc071c78
elaine <emuhombe@gmail.com>
2022-09-13 07:03:48 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
-/*
-*main - Entry point
-*
-* Return: Always 0 (Success)
-*/
 #include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: program that prints exactly: 
+ * 'Programming is like building a multilingual puzzle'
+ * and displays it in a new line
+ *
+ * Return: Always 0 (Success)
+ *
+ */
 
 int main(void)
 {
