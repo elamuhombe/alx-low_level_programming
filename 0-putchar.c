d4bdb27f1866a4d5f6f726f71dfd1d6b9b58104f
elaine <emuhombe@gmail.com>
2022-09-14 11:26:41 -0700

    print _putchar

---
+++ 0-putchar.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *_putchar () _putchar Entryy point
+ *
+ * Description: program that prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int _putchar()
+{
+	putchar("_putchar");
+	return 0;
+}
