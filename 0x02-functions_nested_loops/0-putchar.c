117e606f169ef782445c5ae8a28a82158e512237
elaine <emuhombe@gmail.com>
2022-09-15 03:19:10 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+/**
+ * int _putchar() - _putchar Entry point
+ *
+ * Description: Prints _putchar()
+ *
+ * Return: Always 0 (Success)
+ */
+int _putchar(char _putchar)
+{
+	putchar(_putcahar);
+	return 0;
+}
